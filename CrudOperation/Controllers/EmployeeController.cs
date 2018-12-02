94f5a1918c1c0e168bdd434bd244d1e21244f5ca
Omnia27 <omniaiti38@gmail.com>
2018-12-01 18:10:24 -0800

    commit all

---
+++ CrudOperation/Controllers/EmployeeController.cs
@@ -0,0 +1,89 @@
+﻿using CrudOperation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOperation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        private CompanyEntities db =  new CompanyEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAllEmp()
+        {
+            return View(GetAllEmployees());
+        }
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            using (CompanyEntities db = new CompanyEntities())
+            {
+                return db.Employees.ToList<Employee>();
+            }
+
+        }
+    
+       [Route("Employee/AddOrEdit/{id}")]
+   
+        public ActionResult AddOrEdit(int id=0)
+        {
+            Employee Emp = new Employee();
+            if (id != 0)
+            {
+              Emp  = db.Employees.Find(id);
+            }
+            return View(Emp);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee Emp)
+        {
+            if (Emp.ImageUpload != null)
+
+            {
+                string filename = Path.GetFileNameWithoutExtension(Emp.ImageUpload.FileName);
+                string extension = Path.GetExtension(Emp.ImageUpload.FileName);
+                filename = filename + DateTime.Now.ToString("mmyydd") + extension;
+                Emp.ImagePath= "~/AppFiles/Images/" + filename;
+                Emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), filename));
+            }
+            using (CompanyEntities db=new CompanyEntities())
+            { if (Emp.EmployeeID == 0) {
+                    db.Employees.Add(Emp);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Entry(Emp).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+        public ActionResult Delete(int?id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("ViewAllEmp");
+        }
+
+        public ActionResult Details(int ?id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return View(employee);
+        }
+    }
+}
---
+++ CrudOperation/Scripts/Scripts.js
@@ -0,0 +1,68 @@
+﻿function ShowImagePerview(ImageUploader, PerviewImage) {
+    if (ImageUploader.files && ImageUploader.files[0]) {
+        var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $(PerviewImage).attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(ImageUploader.files[0]);
+
+        }
+
+}
+function JquaryAjaxPost(form) {
+    $.validator.unobtrusive.parse($(form));
+    if ($(form).valid()) {
+        var ajaxconfig = {
+            type: "POST",
+            url: from.action,
+            data: new FromData(from),
+            success: function (respone) {
+                $("#firsttab").html(respone);
+                refreshaddnewtab($(form).attr("data_restUrl"), true);
+            },
+            dataType: dataType
+        }
+        if ($(form).attr("enctype") == multipart / form - data) {
+            ajaxconfig["contentType"] = false;
+            ajaxconfig["processData"] = false;
+        }
+        $.ajax(ajaxconfig);
+
+    }
+    return false;
+
+}
+function refreshaddnewtab(restUrl, showViewTab) {
+    $.ajax({
+        type:"GET",
+        url: restUrl, sucess: function (response) {
+            $("#secondtab").html(respone);
+            $("ul.nav.nav-tabs a:eq(1)").html("Add New");
+            if (showViewTab) {
+                $("ul.nav.nav-tabs a:eq(0)").tab("show");
+            }
+        }
+    });
+   
+}
+function Edit(Url) {
+    debugger;
+    $.ajax({
+        type:"GET",
+        url: Url,
+        success: function (respone) {
+            alert('Success');
+            $("#secondtab").html(respone);
+            $("ul.nav.nav-tabs a:eq(1)").html("Edit");
+
+            $("ul.nav.nav-tabs a:eq(1)").tab("show");
+        },
+        error: function () {
+            alert('A error');
+
+        }
+    })
+        ;
+}
